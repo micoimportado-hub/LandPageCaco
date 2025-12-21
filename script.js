72a53c0977963e5e8c7938a34241e912ade8b897
cacoOmoço <micoimportadora@gmail.com>
2025-12-21 15:21:34 -0300

    Conectando o Git ao GitHub

---
+++ script.js
@@ -0,0 +1,15 @@
+const items = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active');
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+items.forEach(item => observer.observe(item));
+
